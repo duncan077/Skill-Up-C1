3bed6f70ab9164c8f3c79ea85e546b5285f10981
duncan077 <77492620+duncan077@users.noreply.github.com>
2022-11-01 21:40:02 -0300

    Revert "Feature/us 161"

--- AlkemyWallet/Program.cs
+++ AlkemyWallet/Program.cs
@@ -1,16 +1,10 @@
 using AlkemyWallet.Core.Interfaces;
 using AlkemyWallet.Core.Services;
-using AlkemyWallet.DataAccess;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = builder.Configuration.GetConnectionString("WalletDBConnection");
-
-
-
 // Add services to the container.
-builder.Services.AddDbContext<WalletDbContext>(o => o.UseSqlServer(connString));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
