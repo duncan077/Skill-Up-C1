488d2b3b1a2cb5208da0c34ed0698c4185aec517
Braian Velarde <braianvelarde@gmail.com>
2022-11-02 18:37:49 -0300

    Add User Entity

--- AlkemyWallet/Entities/UserEntity.cs
+++ AlkemyWallet/Entities/UserEntity.cs
@@ -1,6 +1,25 @@
-﻿namespace AlkemyWallet.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlkemyWallet.Entities
 {
-    public class UserEntity
+    public class UserEntity : EntityBase
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int Points { get; set; }
+
+        [ForeignKey("role_id")]
+        public virtual RoleEntity Role { get; set; }
+
+        [Column("Id")]
+        public int RoleId { get; set; }
     }
 }
