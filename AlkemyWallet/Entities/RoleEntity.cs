2d98d4327d1fd5b9644123dce8be3cd95a90f748
Pablo Clavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-11-02 17:55:41 -0300

    Modelo, falta migracion

---
+++ AlkemyWallet/Entities/RoleEntity.cs
@@ -0,0 +1,14 @@
+﻿using AlkemyWallet.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlkemyWallet.Entities
+{
+    public class RoleEntity : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+
+    }
+
+}
