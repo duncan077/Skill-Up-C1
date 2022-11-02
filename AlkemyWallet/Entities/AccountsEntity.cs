9d6d36517f5088bda8c552453eb2f1f12642a221
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-02 09:20:06 -0300

    Account entity is created

---
+++ AlkemyWallet/Entities/AccountsEntity.cs
@@ -0,0 +1,19 @@
+﻿using AlkemyWallet.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlkemyWallet.Entities
+{
+    public class AccountsEntity : BaseEntity
+    {
+        public DateTime creationDate { get; set; }
+        public decimal money { get; set; }
+        public bool isBlocked { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual UserEntity userId { get; set; }
+
+    }
+
+}
+
+
