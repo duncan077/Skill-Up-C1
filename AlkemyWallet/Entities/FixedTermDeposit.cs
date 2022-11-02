75e1ac76bdd0fbb6fd7e8927eec6af44375d8aea
santy1493 <santy1493@gmail.com>
2022-11-02 10:14:45 -0300

    Create FixedTermDeposit model

---
+++ AlkemyWallet/Entities/FixedTermDeposit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace AlkemyWallet.Entities
+{
+    public class FixedTermDeposit : BaseEntity
+    {
+     
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+
+
+        public decimal Amount { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ClosingDate { get; set; }
+
+    }
+}
