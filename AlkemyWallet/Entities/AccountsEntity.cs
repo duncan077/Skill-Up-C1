dbb9040a0c107bf98bd576986db7cd828cfa1ff5
Lidio Guedez <112365426+lidioguedez@users.noreply.github.com>
2022-11-02 09:49:19 -0300

    Crear entidad y migracion de Accounts (#4)
    * Account entity is created
    
    * Baseentity is created
    
    Co-authored-by: Lidio Guedez <112365426lidioguedez@users.noreply.github.com>

---
+++ AlkemyWallet/Entities/AccountsEntity.cs
@@ -0,0 +1,19 @@
+﻿using AlkemyWallet.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlkemyWallet.Entities
+{
+    public class AccountsEntity
+    {
+        public DateTime CreationDate { get; set; }
+        public decimal Money { get; set; }
+        public bool IsBlocked { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual UserEntity UserId { get; set; }
+
+    }
+
+}
+
+
