b48af6093b2355262f5192c031edff2f399d8155
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-02 21:30:11 -0300

    Creating generic repository

---
+++ AlkemyWallet/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace AlkemyWallet.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //getAll, getById, insert, delete y update
+
+        Task<IReadOnlyList<T>> getAll();
+        Task<T> GetByIdAsync(int id);
+        Task<int> UpdateAsinc(T entity);
+        Task<int> InsertAsinc(T entity);
+        void DeleteAsinc(T entity);
+
+    }
+}
