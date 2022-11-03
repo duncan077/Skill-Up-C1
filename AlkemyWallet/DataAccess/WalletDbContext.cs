d0ad6d52202afe155630a146f4c3ee18dad74a2a
Diego Cateura <dcateura@gmail.com>
2022-11-03 18:34:57 -0300

    Agregado el Role EntitySeeder para popular en Migration con los dos roles predeterminados. Tambien el wallet DBContext necesario para que funcione.

--- AlkemyWallet/DataAccess/WalletDbContext.cs
+++ AlkemyWallet/DataAccess/WalletDbContext.cs
@@ -1,6 +1,28 @@
-﻿namespace AlkemyWallet.DataAccess
+﻿using AlkemyWallet.DataAccess.DataSeed;
+using AlkemyWallet.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlkemyWallet.DataAccess
 {
-    public class WalletDbContext
+    public class WalletDbContext:DbContext
     {
+
+        public  DbSet<RoleEntity> Roles { get; set; }
+        public  DbSet<AccountEntity> Accounts { get; set; }
+        public  DbSet<UserEntity> Users { get; set; }
+
+        public WalletDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder) {
+
+            RoleEntitySeeder.ConfigureMyEntity(modelbuilder);
+
+            base.OnModelCreating(modelbuilder);
+        
+        }
+
     }
 }
