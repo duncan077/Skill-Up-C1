1a284b5eabd13af89d1eb3129d008f61825cb772
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-02 21:44:50 -0300

    Create GenericRepository Class

--- AlkemyWallet/Repositories/Interfaces/IGenericRepository.cs
+++ AlkemyWallet/Repositories/Interfaces/IGenericRepository.cs
@@ -7,7 +7,7 @@
         Task<IReadOnlyList<T>> getAll();
         Task<T> getById(int id);
         Task<int> insert(T entity);
-        void delete(T entity);
+        Task delete(T entity);
         Task<int> update(T entity);
 
     }
