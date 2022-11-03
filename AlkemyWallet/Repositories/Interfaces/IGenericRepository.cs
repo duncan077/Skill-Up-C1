50852f5f2a4f6ff044e9f1013febd56f6575e3c2
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-02 21:35:49 -0300

    Fixes

--- AlkemyWallet/Repositories/Interfaces/IGenericRepository.cs
+++ AlkemyWallet/Repositories/Interfaces/IGenericRepository.cs
@@ -5,10 +5,10 @@
         //getAll, getById, insert, delete y update
 
         Task<IReadOnlyList<T>> getAll();
-        Task<T> GetByIdAsync(int id);
-        Task<int> UpdateAsinc(T entity);
-        Task<int> InsertAsinc(T entity);
-        void DeleteAsinc(T entity);
+        Task<T> getById(int id);
+        Task<int> insert(T entity);
+        void delete(T entity);
+        Task<int> update(T entity);
 
     }
 }
