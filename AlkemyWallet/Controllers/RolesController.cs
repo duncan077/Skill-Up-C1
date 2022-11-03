f59e23a0f1094c6fd92bc0d6111a63c3f636ce2c
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-03 09:41:54 -0300

    Created RolesController and RolesRepository

---
+++ AlkemyWallet/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using AlkemyWallet.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlkemyWallet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public RolesController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
--- AlkemyWallet/Repositories/UnitOfWork.cs
+++ AlkemyWallet/Repositories/UnitOfWork.cs
@@ -1,8 +1,30 @@
-﻿using AlkemyWallet.Repositories.Interfaces;
+﻿using AlkemyWallet.DataAccess;
+using AlkemyWallet.Entities;
+using AlkemyWallet.Repositories.Interfaces;
 
 namespace AlkemyWallet.Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private IGenericRepository<RoleEntity> _rolesRepository;
+        private WalletDbContext _walletDbContext;
+
+        public UnitOfWork(WalletDbContext walletDbContext)
+        {
+            _walletDbContext = walletDbContext;
+        }
+
+        public IGenericRepository<RoleEntity> RolesRepository
+        {
+            get
+            {
+                if (_rolesRepository == null)
+                {
+                    _rolesRepository = new GenericRepository<RoleEntity>(_walletDbContext);
+                }
+                return _rolesRepository;
+            }
+        }
+
     }
 }
