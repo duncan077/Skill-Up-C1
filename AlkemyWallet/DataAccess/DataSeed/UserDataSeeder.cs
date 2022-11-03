3366d79f8a770178dbafc1598fefaa4544558fac
Duncan <dcacerescartasso@frba.utn.edu.ar>
2022-11-03 15:51:23 -0300

    Fixed Id

--- AlkemyWallet/DataAccess/DataSeed/UserDataSeeder.cs
+++ AlkemyWallet/DataAccess/DataSeed/UserDataSeeder.cs
@@ -9,10 +9,10 @@ namespace AlkemyWallet.DataAccess.DataSeed
         public static void UserDataSeed(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserEntity>().HasData(
-    new { FirstName="Duncan", LastName="Caceres", RoleId=0,Points=0,Email="dcacerescartasso@gmail.com",Password="test1234" },
-    new { FirstName = "Diego", LastName = "Rodrigues", RoleId = 1, Points = 425, Email = "diegor89@gmail.com", Password = "DiegoTest333" },
-    new { FirstName = "Lucas", LastName = "Gonzales", RoleId = 1, Points = 5245, Email = "lgonzales53@gmail.com", Password = "Boca4784" },
-    new { FirstName = "admin", LastName = "admin", RoleId = 0, Points = 0, Email = "alkwallet@gmail.com", Password = "admin1234" });
+    new { Id=0,FirstName="Duncan", LastName="Caceres", RoleId=0,Points=0,Email="dcacerescartasso@gmail.com",Password="test1234" },
+    new { Id=1, FirstName = "Diego", LastName = "Rodrigues", RoleId = 1, Points = 425, Email = "diegor89@gmail.com", Password = "DiegoTest333" },
+    new { Id=2, FirstName = "Lucas", LastName = "Gonzales", RoleId = 1, Points = 5245, Email = "lgonzales53@gmail.com", Password = "Boca4784" },
+    new { Id=3, FirstName = "admin", LastName = "admin", RoleId = 0, Points = 0, Email = "alkwallet@gmail.com", Password = "admin1234" });
         }
     }
 }
