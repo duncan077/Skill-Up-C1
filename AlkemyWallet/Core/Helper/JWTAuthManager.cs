8cfb929528fcb2a81ac9bc525dd0e8d4e7a39f9f
manealucacristian <luca.cmanea@gmail.com>
2022-11-04 22:43:52 -0300

    Borrador JWT

---
+++ AlkemyWallet/Core/Helper/JWTAuthManager.cs
@@ -0,0 +1,65 @@
+﻿using AlkemyWallet.Core.Interfaces;
+using AlkemyWallet.Core.Services;
+using AlkemyWallet.DataAccess;
+using AlkemyWallet.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AlkemyWallet.Core.Helper
+{
+    public class JWTAuthManager
+    {
+        private IConfiguration _configuration;
+        public JWTAuthManager(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+
+        private byte[] CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return passwordHash;
+            }
+        }
+
+        private string CreateToken(string userName)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userName)
+            };
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings").Value));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Token").GetChildren().ToString()));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: credentials);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+
+                var computeddHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeddHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
+
--- AlkemyWallet/Program.cs
+++ AlkemyWallet/Program.cs
@@ -3,7 +3,13 @@ using AlkemyWallet.Core.Services;
 using AlkemyWallet.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using AlkemyWallet.DataAccess.DataSeed;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
+var myAllowSpecificOrigins = "myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("WalletDbConn");
 
@@ -12,8 +18,38 @@ builder.Services.AddDbContext<WalletDbContext>(o => o.UseSqlServer(connString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme(\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: myAllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+        //.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("My clave personalizada para la app")),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 var app = builder.Build();
 
