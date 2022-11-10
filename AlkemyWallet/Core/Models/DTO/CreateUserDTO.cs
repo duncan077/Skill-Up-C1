bfbe90dd1145dfca5525d785c28b9370c63edb04
Braian Velarde <braianvelarde@gmail.com>
2022-11-10 15:08:51 -0300

    Create user model dto

---
+++ AlkemyWallet/Core/Models/DTO/CreateUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using AlkemyWallet.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlkemyWallet.Core.Models.DTO
+{
+    public class CreateUserDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
