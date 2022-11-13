9ed72bbf7d17ca2b9b664989f2a1e612a716928c
Duncan <dcacerescartasso@frba.utn.edu.ar>
2022-11-13 16:00:19 -0300

    Eliminado DTO innecesario

--- AlkemyWallet/Core/Models/DTO/TransactionDTO.cs
+++ AlkemyWallet/Core/Models/DTO/TransactionDTO.cs
@@ -6,26 +6,30 @@ namespace AlkemyWallet.Core.Models.DTO
 {
     public class TransactionDTO
     {
-        public int Id { get; set; }
-       public decimal Amount { get; set; }
+        [Required]
+        public int Id;
+        [Required]
+        public bool IsDeleted { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
         [MinLength(4)]
         [MaxLength(50)]
         public string Concept { get; set; }
+        [Required]
         public DateTime Date { get; set; }
 
-        public string Types { get; set; } = String.Empty;
-      
+        public string Types { get; set; } = "Payment";
 
-      
+
+        [Required]
         public int UserId { get; set; }
-      
+        [Required]
         public int AccountId { get; set; }
-     
-       
+
+        [Required]
         public int ToAccountId { get; set; }
-    
 
 
-     
+
     }
 }
