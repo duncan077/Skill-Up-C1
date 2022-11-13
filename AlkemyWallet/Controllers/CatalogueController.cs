edf5482415da40028746446fa86b0d4a20cb5522
manealucacristian <luca.cmanea@gmail.com>
2022-11-13 12:26:34 -0300

    Post y validacion Catalogue

--- AlkemyWallet/Controllers/CatalogueController.cs
+++ AlkemyWallet/Controllers/CatalogueController.cs
@@ -1,6 +1,7 @@
 ﻿using AlkemyWallet.Core.Interfaces;
 using AlkemyWallet.Core.Models.DTO;
 using AlkemyWallet.Core.Services;
+using AlkemyWallet.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -54,5 +55,29 @@ namespace AlkemyWallet.Controllers
             }
             return Ok(catalogue);
         }
+
+        private bool ValidateStirng(string cadena, int min, int max)
+        {
+            return (!string.IsNullOrEmpty(cadena)) && cadena.Length >= min && cadena.Length <=max;
+        }
+
+        private bool validateCatalogue(CatalogueEntity catalogue)
+        {
+            return (catalogue.Points != null && catalogue.Points >= 0) && (ValidateStirng(catalogue.Image,4,50) && (ValidateStirng(catalogue.ProductDescription,4,200)));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> PostCatalogue([FromBody] CatalogueEntity catalogue)
+        {
+            if (validateCatalogue(catalogue)/*!(catalogue is null) ||ModelState.IsValid*/)
+            {
+                await _catalogueService.insert(_mapper.Map<CatalogueEntity>(catalogue));
+                return Ok(new { message = "agregado correctamente", Code=200});
+            }
+            else return BadRequest(new { message = "Error al agregar el registro" , Code=500});
+        }
+
+
     }
 }
